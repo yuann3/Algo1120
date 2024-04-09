b012e38d4794cf9d87912be09cf75c2c8521280b
Yiyuan <etherealdt@gmail.com>
2024-04-09 14:42:18 +0800

    fix bug in python solution

--- static_arrays/python/26. Remove_Duplicates_from_Sorted_Array.py
+++ static_arrays/python/26. Remove_Duplicates_from_Sorted_Array.py
@@ -3,15 +3,15 @@ from typing import List
 class Solution:
   def removeDuplicates(self, nums: List[int]) -> int:
     # Initialize a pointer to track the position of the last unique element
-    l = 1;
-    
+    L = 1;
+
     # Iterate over the array starting from the second element
-    for r in range(1, len(nums)):
+    for R in range(1, len(nums)):
       # If the current element is not equal to the previous one
-      if nums[r] != nums[r - 1]:
+      if nums[R] != nums[R - 1]:
         # Move the current element to the position after the last unique element
-        nums[l] = nums[r]
+        nums[L] = nums[R]
         # Increment the pointer
-        l + 1
+        L += 1
     # Return the number of unique elements
-    return l
+    return L
