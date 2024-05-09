fd9585af7a00d4757e4113f8845ad0efb0babb76
Yiyuan <etherealdt@gmail.com>
2024-05-09 17:49:22 +0800

    change to COMP1120

---
+++ Lab1120/Lab 1 Intro SENG1120/fibonacciNums.cpp
@@ -0,0 +1,26 @@
+// FibonacciNums.cpp
+// Author: Yiyuan Li (C3434681)
+// Date: 08-05-2024
+#include <iostream>
+
+void PrintFibonacciNumbers(int limit) {
+    int f1 = 0, f2 = 1;
+
+    if (limit < 1)
+        return;
+
+    while (f1 <= limit){
+        std::cout << f1 << " ";
+        int next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+    }
+}
+
+int main() {
+    int limit;
+    std::cout << "Enter a number: ";
+    std::cin >> limit;
+    PrintFibonacciNumbers(limit);
+    return 0;
+}
