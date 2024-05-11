5a991cdff73d57750e3f252098bb3fda23a77add
Yiyuan <etherealdt@gmail.com>
2024-05-11 23:19:57 +0800

    finish lc_1929

---
+++ LeetCode/arrays/lc_1929.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> getConcatenation(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans;
+    ans.reserve(2 * n);
+
+    ans.insert(ans.end(), nums.begin(), nums.end());
+    ans.insert(ans.end(), nums.begin(), nums.end());
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 2, 1};
+    vector<int> ans = getConcatenation(nums);
+
+    cout << "Duplicated array: ";
+        for (int num : ans) {
+            cout << num << " ";
+        }
+        cout << endl;
+
+        return 0;
+}
