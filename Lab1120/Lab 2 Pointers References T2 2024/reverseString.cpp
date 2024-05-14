d20c2b61e9a2cf79d23205a8f4ac20273b4d7843
Yiyuan <etherealdt@gmail.com>
2024-05-14 14:18:35 +0800

    finish reverse string

--- Lab1120/Lab 2 Pointers References T2 2024/reverseString.cpp
+++ Lab1120/Lab 2 Pointers References T2 2024/reverseString.cpp
@@ -5,11 +5,18 @@
 #include <iostream>
 #include <string>
 
-std::string reverse()
+std::string reverse(const std::string& s)
 {
+    std::string reversed_string;
+    for (int i = s.size() - 1; i >= 0; i--)
+        reversed_string += s[i];
+    return reversed_string;
 }
 
 int main()
 {
-    return 0;
+    std::string original = "university of newcastle";
+    std::string reversed = reverse(original);
+    std::cout << "Original: " << original << std::endl;
+    std::cout << "Reversed: " << reversed << std::endl;
 }
